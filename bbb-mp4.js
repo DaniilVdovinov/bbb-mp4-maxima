d05cd59bcba3d6658a78256963b03c474e5fd309
damir.iusupov.w <damir.iusupov.w@gmail.com>
2023-07-05 10:23:59 +0300

    try to fix bug with camera

--- bbb-mp4.js
+++ bbb-mp4.js
@@ -129,14 +129,14 @@ async function main() {
         await page.click('button[class=vjs-big-play-button]', {waitUntil: 'domcontentloaded'});
 
         await page.waitForTimeout(200)
-        await page.click('button[class=vjs-playing]', {waitUntil: 'domcontentloaded'});
-        await page.click('button[class=skip-back]', {waitUntil: 'domcontentloaded'});
+        await page.click('button.vjs-playing', {waitUntil: 'domcontentloaded'});
+        await page.click('button.skip-back', {waitUntil: 'domcontentloaded'});
 
         await page.waitForTimeout(5 * 1000)
-        await page.click('button[class=vjs-paused]', {waitUntil: 'domcontentloaded'});
+        await page.click('button.vjs-paused', {waitUntil: 'domcontentloaded'});
         await page.$eval('.vjs-control-bar', element => element.style.opacity = "0");
 
-        page.waitForSelector('div#vjs_video_3.vjs-playing');
+        await page.waitForSelector('div#vjs_video_3.vjs-playing');
 
         //  Start capturing screen with ffmpeg
         const ls = child_process.spawn('sh', ['ffmpeg-cmd.sh', ' ',
